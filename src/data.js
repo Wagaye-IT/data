cfa892eadfea904db7d7743485df9e08971ad1e5
wagay <tenywag@yahoo.com>
2024-02-16 12:19:22 -0500

    My training on data populationg

---
+++ src/data.js
@@ -0,0 +1,67 @@
+
+export const data = [
+    {
+    "_id": "65b70dbe1bb28f6417476adf",
+    "title": "Cyber Security 2021",
+    "authors": " Hailemichael",
+    "publisher": "Bahirdar University",
+    "publishYear": "2024",
+    "__v": 0
+    },
+    {
+    "_id": "65b70de31bb28f6417476ae1",
+    "title": "Sememen Fiction",
+    "authors": "Sisay Negusu ",
+    "publisher": "Kuraz press",
+    "publishYear": "Dec23 ,2000",
+    "__v": 0
+    },
+    {
+    "_id": "65b70e031bb28f6417476ae3",
+    "title": "Fiker Eskemekaber",
+    "authors": "Haddis Alemayehu",
+    "publisher": "Kuraz Prining",
+    "publishYear": "Mar 23,1985",
+    "__v": 0
+    },
+    {
+    "_id": "65b70e5a1bb28f6417476ae9",
+    "title": "Shotelayu Selaye",
+    "authors": "Yirga Zewdu",
+    "publisher": "Birhana Selam",
+    "publishYear": " 1970",
+    "__v": 0
+    },
+    {
+    "_id": "65b71d6a9bcd96a02e086b45",
+    "title": "Rich Dad & Poor Dad",
+    "authors": "Robert  Kiyosaki",
+    "publisher": "Book world",
+    "publishYear": "2017",
+    "__v": 0
+    },
+    {
+    "_id": "65b722df9bcd96a02e086b9e",
+    "title": "Great Books",
+    "authors": "Duglas",
+    "publisher": "SouthWest",
+    "publishYear": "2015",
+    "__v": 0
+    },
+    {
+    "_id": "65c188cf1ed87f9d0c0fb5dc",
+    "title": "SQL Introduction",
+    "authors": "Getachew",
+    "publisher": "Practical It",
+    "publishYear": "May 20,2008",
+    "__v": 0
+    },
+    {
+    "_id": "65c6eac2032a055e4e050e9c",
+    "title": "effe",
+    "authors": "fef",
+    "publisher": "efe",
+    "publishYear": "efe",
+    "__v": 0
+    }
+    ]
